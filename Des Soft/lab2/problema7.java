41ac50ec924b2480aace2090951ce0ee5a2958d2
SyntaxMistake <barahonajose279@gmail.com>
2024-10-08 02:21:34 -0500

    Agregue el problema7

---
+++ Des Soft/lab2/problema7.java
@@ -0,0 +1,26 @@
+public class problema7 {
+    private double hora, sueldo;
+
+    public void asignarValores(double h1, double s1){
+        hora=h1;
+        sueldo=s1;
+    }
+    public double calSueldoHora(){
+        double sueldoTt=0;
+        if(hora <=40){
+            sueldoTt=sueldo*hora;
+        }
+        else{
+            sueldoTt=40*sueldo*hora*1.5;
+        }
+        return sueldoTt;
+    }
+}
+class SueldoPorHora{
+    public static void main(String[] args) {
+        problema7 obj = new problema7();
+        obj.asignarValores(45, 50.75);
+        obj.calSueldoHora();
+        System.out.println("El saldo de Juan Perez es de "+ obj.calSueldoHora());
+    }
+}
